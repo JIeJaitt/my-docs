4720f00c52a101e6b7740570182f077b9ca6f341
JIeJaitt <498938874@qq.com>
2026-04-23 18:16:54 +0800

    chore: update dependencies and improve .gitignore for environment files

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -163,7 +163,7 @@ export default hopeTheme(
               if (a === b) return 0;
               return a < b ? 1 : -1;
             },
-            layout: "BlogType",
+            layout: "Blog",
           },
         ],
       },
@@ -181,62 +181,62 @@ export default hopeTheme(
         prefix: "fa6-solid:",
       },
 
-      pwa: {
-        favicon: "/favicon.ico",
-        cacheHTML: true,
-        cacheImage: true,
-        appendBase: true,
-        apple: {
-          icon: "/assets/icon/apple-icon-152.png",
-          statusBarColor: "black",
-        },
-        msTile: {
-          image: "/assets/icon/ms-icon-144.png",
-          color: "#ffffff",
-        },
-        manifest: {
-          icons: [
-            {
-              src: "/assets/icon/chrome-mask-512.png",
-              sizes: "512x512",
-              purpose: "maskable",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-192.png",
-              sizes: "192x192",
-              purpose: "maskable",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-          ],
-          shortcuts: [
-            {
-              name: "Demo",
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              short_name: "Demo",
-              url: "/demo/",
-              icons: [
-                {
-                  src: "/assets/icon/guide-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-            },
-          ],
-        },
-      },
+      // pwa: {
+      //   favicon: "/favicon.ico",
+      //   cacheHTML: true,
+      //   cacheImage: true,
+      //   appendBase: true,
+      //   apple: {
+      //     icon: "/assets/icon/apple-icon-152.png",
+      //     statusBarColor: "black",
+      //   },
+      //   msTile: {
+      //     image: "/assets/icon/ms-icon-144.png",
+      //     color: "#ffffff",
+      //   },
+      //   manifest: {
+      //     icons: [
+      //       {
+      //         src: "/assets/icon/chrome-mask-512.png",
+      //         sizes: "512x512",
+      //         purpose: "maskable",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-mask-192.png",
+      //         sizes: "192x192",
+      //         purpose: "maskable",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-512.png",
+      //         sizes: "512x512",
+      //         type: "image/png",
+      //       },
+      //       {
+      //         src: "/assets/icon/chrome-192.png",
+      //         sizes: "192x192",
+      //         type: "image/png",
+      //       },
+      //     ],
+      //     shortcuts: [
+      //       {
+      //         name: "Demo",
+      //         // eslint-disable-next-line @typescript-eslint/naming-convention
+      //         short_name: "Demo",
+      //         url: "/demo/",
+      //         icons: [
+      //           {
+      //             src: "/assets/icon/guide-maskable.png",
+      //             sizes: "192x192",
+      //             purpose: "maskable",
+      //             type: "image/png",
+      //           },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      // },
 
       seo:
         hostname === "https://docs.jiejaitt.top"
